f864c1f4e28e13de62b1b7591c49862f60f67860
theawless <theawless@gmail.com>
2017-10-07 13:22:46 +0530

    Add mongo.py
    Allows fetching tweet and user data

---
+++ mongo.py
@@ -0,0 +1,53 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+vegas_db = client.vegas
+tweets_collection = vegas_db.tweets
+users_collection = vegas_db.users
+
+
+def get_tweets():
+    tweets_cursor = tweets_collection.find().sort("timestamp_ms", pymongo.ASCENDING).limit(10)
+    tweets = list(tweets_cursor)
+    return tweets
+
+
+def save_users_from_tweets(tweets):
+    users = {}
+    for tweet in tweets:
+        # author
+        user = tweet["user"]
+        users[user["id_str"]] = user
+
+        # retweet
+        if "retweeted_status" in tweet:
+            user = tweet["retweeted_status"]["user"]
+            users[user["id_str"]] = user
+
+        # reply
+        in_reply_to_user_id_str = tweet["in_reply_to_user_id_str"]
+        if in_reply_to_user_id_str is not None:
+            if in_reply_to_user_id_str not in users:
+                users[in_reply_to_user_id_str] = {"id_str": in_reply_to_user_id_str}
+
+        # mentions
+        for mention in tweet["entities"]["user_mentions"]:
+            if mention["id_str"] not in users:
+                users[mention["id_str"]] = {"id_str": mention["id_str"]}
+    users_collection.insert_many(users.values())
+
+
+def clean_tweets():
+    tweets_collection.delete_many({"limit": {"$exists": "true"}})
+
+
+def get_users():
+    users_cursor = users_collection.find()
+    users = list(users_cursor)
+    return users
+
+
+if __name__ == '__main__':
+    clean_tweets()
+    save_users_from_tweets(get_tweets())
